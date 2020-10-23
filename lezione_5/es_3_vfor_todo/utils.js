89106547a9310346b360c73de5138e5508850a5c
michipase <michipase@gmail.com>
2020-10-23 11:46:43 +0200

    added local storage

--- lezione_5/es_3_vfor_todo/utils.js
+++ lezione_5/es_3_vfor_todo/utils.js
@@ -15,6 +15,16 @@ let app = new Vue({
             this.archived.push(this.list[x]);
             this.list.splice(x,1);
             this.newEl = '';
-        }
+        },
+        save(){
+            const data = {todo: this.list, archived: this.archived};
+            window.localStorage.setItem('savedTodo',JSON.stringify(data));
+        },
+        getTodo(){
+            let data = JSON.parse(window.localStorage.getItem('savedTodo'));
+
+            this.list = data.todo;
+            this.archived = data.archived;
+        },
     }
 });
