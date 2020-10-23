cf4cf73bd8f8bbd9f0df4e4c3cfec289f22af736
michipase <michipase@gmail.com>
2020-10-23 12:11:38 +0200

    style fixes

--- lezione_5/es_3_vfor_todo/utils.js
+++ lezione_5/es_3_vfor_todo/utils.js
@@ -5,6 +5,8 @@ let app = new Vue({
             list:[],
             archived: [],
             newEl: '',
+            message: '',
+            disable: false,
         }
     },
     methods: {
@@ -19,12 +21,22 @@ let app = new Vue({
         save(){
             const data = {todo: this.list, archived: this.archived};
             window.localStorage.setItem('savedTodo',JSON.stringify(data));
+            this.message = 'salvati gli attuali task :)'
+            this.disable = true;
+            setTimeout(_ => {this.message = ''; this.disable = false}, 5000)
         },
         getTodo(){
             let data = JSON.parse(window.localStorage.getItem('savedTodo'));
 
-            this.list = data.todo;
-            this.archived = data.archived;
+            if(data.todo !== [] && data.archive !== []){
+                this.list = data.todo;
+                this.archived = data.archived;
+                this.message = 'Aggiunti i task precedentemente salvati';
+                this.disable = true;
+                setTimeout(_ => {this.message = ''; this.disable = false}, 5000)
+            } else {
+                
+            }
         },
     }
 });
